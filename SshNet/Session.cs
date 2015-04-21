b96dc245b39dcf6f67d0feb3cae9f7c643b99351
Aimeast <lixd3389@gmail.com>
2015-04-21 23:12:34 +0800

    write and read SSH version

---
+++ SshNet/Session.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SshNet
+{
+    public class Session
+    {
+        private const byte Null = 0x00;
+        private const byte CarriageReturn = 0x0d;
+        private const byte LineFeed = 0x0a;
+        private const int MaximumSshPacketSize = LocalChannelDataPacketSize + 3000;
+        private const int InitialLocalWindowSize = LocalChannelDataPacketSize * 32;
+        private const int LocalChannelDataPacketSize = 1024 * 64;
+
+        private readonly Socket _socket;
+#if DEBUG
+        private readonly TimeSpan _timeout = TimeSpan.FromDays(1);
+#else
+        private readonly TimeSpan _timeout = TimeSpan.FromSeconds(30);
+#endif
+        public string ServerVersion { get; private set; }
+        public string ClientVersion { get; private set; }
+
+        public Session(Socket socket)
+        {
+            Contract.Requires(socket != null);
+
+            _socket = socket;
+            ServerVersion = "SSH-2.0-SshNet";
+        }
+
+        public event EventHandler<EventArgs> Disconnected;
+
+        public bool IsConnected { get; private set; }
+
+        public void EstablishConnection()
+        {
+            if (IsConnected)
+                return;
+
+            SetSocketOptions();
+
+            SocketWriteLine(ServerVersion);
+            ClientVersion = SocketReadLine();
+            var clientIdVersions = ClientVersion.Split('-')[1];
+            if (clientIdVersions != "2.0")
+            {
+                // TODO: send disconnect message
+                throw new NotSupportedException(string.Format("Not supported for client SSH version {0}.", clientIdVersions));
+            }
+        }
+
+        public void Disconnect()
+        {
+            _socket.Disconnect(true);
+
+            if (Disconnected != null)
+                Disconnected(this, EventArgs.Empty);
+        }
+
+        private void SetSocketOptions()
+        {
+            const int socketBufferSize = 2 * MaximumSshPacketSize;
+            _socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+            _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendBuffer, socketBufferSize);
+            _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveBuffer, socketBufferSize);
+        }
+
+        private string SocketReadLine()
+        {
+            var encoding = Encoding.ASCII;
+            var buffer = new List<byte>();
+            var data = new byte[1];
+
+            // read data one byte at a time to find end of line and leave any unhandled information in the buffer
+            // to be processed by subsequent invocations
+            do
+            {
+                var asyncResult = _socket.BeginReceive(data, 0, data.Length, SocketFlags.None, null, null);
+                if (!asyncResult.AsyncWaitHandle.WaitOne(_timeout))
+                    throw new TimeoutException(string.Format(CultureInfo.InvariantCulture,
+                        "Socket read operation has timed out after {0:F0} milliseconds.", _timeout.TotalMilliseconds));
+
+                var received = _socket.EndReceive(asyncResult);
+
+                if (received == 0)
+                    // the remote server shut down the socket
+                    break;
+
+                buffer.Add(data[0]);
+            }
+            while (!(buffer.Count > 0 && (buffer[buffer.Count - 1] == LineFeed || buffer[buffer.Count - 1] == Null)));
+
+            if (buffer.Count == 0)
+                return null;
+            else if (buffer.Count == 1 && buffer[buffer.Count - 1] == 0x00)
+                // return an empty version string if the buffer consists of only a 0x00 character
+                return string.Empty;
+            else if (buffer.Count > 1 && buffer[buffer.Count - 2] == CarriageReturn)
+                // strip trailing CRLF
+                return encoding.GetString(buffer.Take(buffer.Count - 2).ToArray());
+            else if (buffer.Count > 1 && buffer[buffer.Count - 1] == LineFeed)
+                // strip trailing LF
+                return encoding.GetString(buffer.Take(buffer.Count - 1).ToArray());
+            else
+                return encoding.GetString(buffer.ToArray());
+        }
+
+        private void SocketWriteLine(string str)
+        {
+            SocketWrite(Encoding.UTF8.GetBytes(str + Environment.NewLine));
+        }
+
+        private byte[] SocketRead(int length)
+        {
+            var receivedTotal = 0;  // how many bytes is already received
+            var buffer = new byte[length];
+
+            do
+            {
+                try
+                {
+                    var receivedBytes = _socket.Receive(buffer, receivedTotal, length - receivedTotal, SocketFlags.None);
+                    if (receivedBytes > 0)
+                    {
+                        receivedTotal += receivedBytes;
+                        continue;
+                    }
+                    throw new SocketException((int)SocketError.ConnectionAborted);
+                }
+                catch (SocketException exp)
+                {
+                    if (exp.SocketErrorCode == SocketError.WouldBlock ||
+                        exp.SocketErrorCode == SocketError.IOPending ||
+                        exp.SocketErrorCode == SocketError.NoBufferSpaceAvailable)
+                    {
+                        // socket buffer is probably empty, wait and try again
+                        Thread.Sleep(30);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            } while (receivedTotal < length);
+
+            return buffer;
+        }
+
+        private void SocketWrite(byte[] data)
+        {
+            SocketWrite(data, 0, data.Length);
+        }
+
+        private void SocketWrite(byte[] data, int offset, int length)
+        {
+            var totalBytesSent = 0;  // how many bytes are already sent
+            var totalBytesToSend = length;
+
+            do
+            {
+                try
+                {
+                    totalBytesSent += _socket.Send(data, offset + totalBytesSent, totalBytesToSend - totalBytesSent,
+                        SocketFlags.None);
+                }
+                catch (SocketException ex)
+                {
+                    if (ex.SocketErrorCode == SocketError.WouldBlock ||
+                        ex.SocketErrorCode == SocketError.IOPending ||
+                        ex.SocketErrorCode == SocketError.NoBufferSpaceAvailable)
+                    {
+                        // socket buffer is probably full, wait and try again
+                        Thread.Sleep(30);
+                    }
+                    else
+                        throw;  // any serious error occurr
+                }
+            } while (totalBytesSent < totalBytesToSend);
+        }
+    }
+}
---
+++ SshNet/SshServer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace SshNet
+{
+    public class SshServer : IDisposable
+    {
+        private readonly object _lock = new object();
+        private readonly List<Session> _sessions = new List<Session>();
+        private bool _isDisposed;
+        private bool _started;
+        private TcpListener _listenser = null;
+
+        public SshServer()
+            : this(new StartingInfo())
+        { }
+
+        public SshServer(StartingInfo info)
+        {
+            Contract.Requires(info != null);
+
+            StartingInfo = info;
+        }
+
+        public StartingInfo StartingInfo { get; private set; }
+
+        public void Start()
+        {
+            lock (_lock)
+            {
+                CheckDisposed();
+                if (_started)
+                    throw new InvalidOperationException("The server is already started.");
+
+                _listenser = StartingInfo.LocalAddress == IPAddress.IPv6Any
+                    ? TcpListener.Create(StartingInfo.Port) // dual stack
+                    : new TcpListener(StartingInfo.LocalAddress, StartingInfo.Port);
+                _listenser.Start();
+                Task.Run(() => AcceptSocket());
+
+                _started = true;
+            }
+        }
+
+        public void Stop()
+        {
+            lock (_lock)
+            {
+                CheckDisposed();
+                if (!_started)
+                    throw new InvalidOperationException("The server is not started.");
+
+                _listenser.Stop();
+
+                foreach (var session in _sessions)
+                {
+                    try
+                    {
+                        session.Disconnect();
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                _isDisposed = true;
+                _started = false;
+            }
+        }
+
+        private void AcceptSocket()
+        {
+            while (true)
+            {
+                try
+                {
+                    var socket = _listenser.AcceptSocket();
+                    Task.Run(() =>
+                    {
+                        var session = new Session(socket);
+                        session.EstablishConnection();
+                        session.Disconnected += (ss, ee) => _sessions.Remove(session);
+                        _sessions.Add(session);
+                    });
+                }
+                catch (InvalidOperationException)
+                {
+                    if (_isDisposed || !_started)
+                        return;
+
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void CheckDisposed()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(GetType().FullName);
+        }
+
+        #region IDisposable
+        public void Dispose()
+        {
+            lock (_lock)
+            {
+                if (_isDisposed)
+                    return;
+                Stop();
+            }
+        }
+        #endregion
+    }
+}
