3608d460c58f294e4f68f352e94fda3806212941
Aimeast <lixd3389@gmail.com>
2015-04-23 23:40:49 +0800

    import key exchange algorithm

---
+++ SshNet/Algorithms/KexAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace SshNet.Algorithms
+{
+    public abstract class KexAlgorithm
+    {
+        protected HashAlgorithm _hashAlgorithm;
+
+        public abstract byte[] CreateKeyExchange();
+
+        public abstract byte[] DecryptKeyExchange(byte[] exchangeData);
+
+        public byte[] ComputeHash(byte[] input)
+        {
+            return _hashAlgorithm.ComputeHash(input);
+        }
+    }
+}
