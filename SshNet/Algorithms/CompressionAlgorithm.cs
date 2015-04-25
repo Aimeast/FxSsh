02fc2bece76d87b53b4c05597d020c30f18d176f
Aimeast <lixd3389@gmail.com>
2015-04-25 13:19:03 +0800

    import compression algorithm

---
+++ SshNet/Algorithms/CompressionAlgorithm.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SshNet.Algorithms
+{
+    public abstract class CompressionAlgorithm
+    {
+        public abstract byte[] Compress(byte[] input);
+
+        public abstract byte[] Decompress(byte[] input);
+    }
+}
---
+++ SshNet/Algorithms/NoCompression.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SshNet.Algorithms
+{
+    public class NoCompression : CompressionAlgorithm
+    {
+        public override byte[] Compress(byte[] input)
+        {
+            return input;
+        }
+
+        public override byte[] Decompress(byte[] input)
+        {
+            return input;
+        }
+    }
+}
