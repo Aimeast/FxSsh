de2352e2542c51d7b7ef61e7a7f60d6425f95f78
Aimeast <lixd3389@gmail.com>
2015-04-25 12:50:18 +0800

    import hmac algorithm

---
+++ SshNet/Algorithms/HmacAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+
+namespace SshNet.Algorithms
+{
+    public class HmacAlgorithm
+    {
+        private readonly KeyedHashAlgorithm _algorithm;
+
+        public HmacAlgorithm(KeyedHashAlgorithm algorithm)
+        {
+            Contract.Requires(algorithm != null);
+
+            _algorithm = algorithm;
+        }
+
+        public int DigestLength
+        {
+            get { return _algorithm.HashSize >> 3; }
+        }
+
+        public byte[] ComputeHash(byte[] input)
+        {
+            return _algorithm.ComputeHash(input);
+        }
+    }
+}
